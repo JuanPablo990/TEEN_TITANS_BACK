3f1f5fc5cc8a758fc6105ad6e78774441df89697
JuanPablo990 <juan.nieto-co@mail.escuelaing.edu.co>
2025-09-16 19:08:00 -0500

    feat: Proyecto: se a creado el proyecto maven

---
+++ src/main/java/eci/edu/dosw/parcial/TEEN_TITANS_BACK/TeenTitansBackApplication.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.parcial.TEEN_TITANS_BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeenTitansBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeenTitansBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/edu/dosw/parcial/TEEN_TITANS_BACK/TeenTitansBackApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.dosw.parcial.TEEN_TITANS_BACK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeenTitansBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
